3c1ab46f1cae2e521eb9e04739f3615428cb3822
mirkodeglinnocenti <deglinnocentimirko@gmail.com>
2023-02-03 15:57:16 +0100

    FEAT Exercise added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,34 @@
 // console.log('Funziona')
+
+//     Esercizio email
+// Crea un array con una lista di email (stringhe)
+// Chiedi all’utente la sua email (con il prompt), controlla che sia nella lista di email,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+// Crea un array con una lista di email (stringhe)
+
+const email = ['email1@gmail.com' , 'email2@gmail.com' , 'email3@gmail.com' , 'email4@gmail.com' ];
+
+// console.log(email);
+
+// Chiedi all’utente la sua email (con il prompt), controlla che sia nella lista di email
+
+let userEmail = prompt ('Inserisci la tua email per verificare che sia in lista');
+
+// console.log(userEmail);
+
+for (let i = 0 ; i < email.length ; i++ ) {
+
+    // console.log( email[i] );
+
+
+    if ( email[i] === userEmail ) {
+        console.log('Email già presente')
+    } else {
+        console.log('Email non presente')
+    }
+
+
+}
